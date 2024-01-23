60aec9b05a6a942562ff7265493464d1540d3dbd
Aman Kumar <aman.kr.2129@gmail.com>
2024-01-23 08:01:30 +0530

    vercel specific deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { config } = require("dotenv");
 const cors = require("cors");
 
 config();
-const port = process.env.PORT || 5500;
+
 const app = express();
 const home = require("./routes/home");
 const spotify = require("./routes/spotify");
@@ -12,10 +12,9 @@ const youtube = require("./routes/youtube");
 app.use(cors());
 app.use(express.json())
 
-app.use(home);
-app.use(spotify);
-app.use(youtube);
+app.use('/api', home);
+app.use('/api', spotify);
+app.use('/api', youtube);
 
-app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}\n`);
-});
+module.exports = app;
+
